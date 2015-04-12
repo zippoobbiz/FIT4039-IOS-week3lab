c003da24506f33e073242c09ca2826bc623240e4
Xiaoduo <zippoobbiz@gmail.com>
2015-04-12 17:45:38 +1000

    Initial Commit

---
+++ week3lab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  week3lab
+//
+//  Created by BigBadEgg on 4/12/15.
+//  Copyright (c) 2015 Xiaoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ week3lab/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  week3lab
+//
+//  Created by BigBadEgg on 4/12/15.
+//  Copyright (c) 2015 Xiaoduo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
